f87137145b83e8f167baeb652d478a35286336b9
Lee-sojung <thwjd3698@gmail.com>
2021-12-31 11:55:00 +0900

    유투브 패널 스타일링

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -6,29 +6,53 @@
 */
 
 import axios from "axios";
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
-function Youtube(){
+function Youtube() {
     let [data, setData] = useState([]);
 
-    const api_key = "AIzaSyAzmrdyn7aRmI1m18va9_agD7NE9--3Rkc";
-    const playListId = "PLGzNH5a9hxjZq5ick2B6ns6t8xLpS5-LF";
+    const api_key = "AIzaSyC7rA3bw12Oogstpe_sGmJFYrmr47anoZ4";
+    const playListId = "PLYOPkdUKSFgX5CgKf68RJzJHec0XEdBNd";
     const num = 4;
     const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${api_key}&playlistId=${playListId}&maxResults=${num}`;
 
-    useEffect(()=>{
+    useEffect(() => {
         axios
-        .get(url)
-        .then(json=>{
-            console.log(json.data.items);
-            setData(json.data.items);
-        })
-    },[]);
+            .get(url)
+            .then(json => {
+                console.log(json.data.items);
+                setData(json.data.items);
+            })
+    }, []);
 
-    return(
-        <main>
+    return (
+        <main className="youtube">
             <div className="inner">
                 <h1><a href="#">Youtube</a></h1>
+                <section className="frame">
+                    {
+                        data.map((item, index) => {
+                            let tit = item.snippet.title;
+                            let tit_len = tit.length;
+                            let desc = item.snippet.description;
+                            let desc_len = desc.length;
+
+                            return (
+                                <article key={index}>
+                                    <div className="inner">
+                                        < div className="txt">
+                                            <h2>{(tit_len > 40) ? tit = tit.substr(0, 40) + "..." : tit}</h2>
+                                            <p>{(desc_len > 150) ? desc = desc.substr(0, 150) + "..." : desc}</p>
+                                        </ div>
+                                        <div className="pic">
+                                            <img src={item.snippet.thumbnails.medium.url} />
+                                        </div>
+                                    </div>
+                                </article>
+                            )
+                        })
+                    }
+                </section>
 
             </div>
         </main>
