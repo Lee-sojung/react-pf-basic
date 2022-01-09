db788bee46cbb5c51ed4f3eca2349ae4b33a99e6
Lee-sojung <thwjd3698@gmail.com>
2022-01-10 07:07:41 +0900

    커뮤니티 뉴스레터 완성

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -43,6 +43,7 @@ function Community() {
                         )
                     })
                 }
+
                 <div className="pagination">
                     <p className="numbers">
                         <button className="on">1</button>
@@ -52,17 +53,19 @@ function Community() {
                         <button>5</button>
                     </p>
                 </div>
+
                 <div className="right">
                     <img className="pic1" src={path + "/img/commu_pic1.jpeg"} />
 
                     <img className="pic2" src={path + "/img/commu_pic9.jpeg"} />
 
                 </div>
+
                 <div className="banner">
                     <div className="b_box">
                         <img className="b_pic" src={path + "/img/bg.jpg"} />
-                        <p>
-                            The opening of the flagship store is meaningful in that it serves as an important opportunity to deepen the link between Louis Vuitton and Korean culture.</p>
+                        <h2>
+                            The opening of the flagship store is meaningful in that it serves as an important opportunity to deepen the link between Louis Vuitton and Korean culture.</h2>
                     </div>
 
                 </div>
@@ -96,6 +99,29 @@ function Community() {
                         <p>
                             Marino added, “The stone material used in the interior flows from the outside, and the dynamism of the huge square contrasts sharply with the baroque glass windows of the building.”</p>
                         <button>MORE</button>
+                    </div>
+                </div>
+
+                <div className="opening">
+                    <div className="open">
+                        <img className="open_box" src={path + "/img/Opening2.jpeg"} />
+                    </div>
+                    <div className="open_txt">
+                        <h2>Louis Vuitton New Bond Street</h2>
+                        <p>On the evening of 23rd October, Louis Vuitton held an event to celebrate the grand reopening of the London Maison. Louis Vuitton Chairman and Chief Executive Officer Michael Burke welcomed the Maison's muse, friends and celebrity guests at the unveiling of the remodeled Louis Vuitton New Bond Street store.<br />
+                            A special cocktail dinner party will include Kylie Minogue, Gal Gadot, Shailene Woodley, Riley Keough, Jason Momoa and Noel Gallagher, among many others. Noel Gallagher, Naomi Scott, Isabelle Huppert, Stacy Martin, Mark Ronson, Raffey Cassidy, Doutzen Kroes, Maisie Williams Williams, Alexa Chung, Poppy Delevingne, Lady Amelia Windsor, Sabine Getty, Yasmin Le Bon and Amber Le Bon and others were present.</p>
+                        <button>MORE</button>
+                    </div>
+                </div>
+
+                <div className="newsletter">
+                    <img className="letter" src={path + "/img/newsletter.jpeg"} />
+                    <div className="letter_txt">
+                        <span>NEWS LETTER</span>
+                        <h1>THE LATEST NEWS FROM THE<br/>MAGAZINE</h1>
+
+                        <button>Subscribe</button>
+                        <p>Be the first to know. Follow us on</p>
                     </div>
                 </div>
 
