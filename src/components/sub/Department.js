847841ebdb20ce3e2a742bf8d8a1b245c7fa6784
Lee-sojung <thwjd3698@gmail.com>
2022-01-12 06:46:42 +0900

    department 제목스타일 변경

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,8 +1,9 @@
 function Department(){
     return(
-        <main>
+        <main className="department">
             <div className="inner">
-                <h1><a href="#">Department</a></h1>
+                <h1><a href="#">DEPARTMENT</a></h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elity cinseterd Doloremque amet recusandae enim voluptatem nemo culpa! Dolor fugit vel, itaque deserunt quae minus quibusdam unde sint initded </p>
             </div>
         </main>
     )
