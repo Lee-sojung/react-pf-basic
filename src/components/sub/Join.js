e32544eedb57977e3a8d9faf6f7c84168066c2e2
Lee-sojung <thwjd3698@gmail.com>
2022-01-17 09:17:15 +0900

    스타일 수정

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,6 +1,6 @@
 function Join(){
     return(
-        <main>
+        <main className="join">
             <div className="inner">
                 <h1><a href="#">Join</a></h1>
             </div>
--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -72,7 +72,7 @@ function Location() {
     return (
         <main className="location">
             <div className="inner">
-                <h1><a href="#">Location</a></h1>
+                <h1><a href="#">LOCATION</a></h1>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elity cinseterd Doloremque amet recusandae enim voluptatem nemo culpa! Dolor fugit vel, itaque deserunt quae minus quibusdam unde sint initded </p>
 
                 <div className="ro_l">
